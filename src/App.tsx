2511b6c6dac13631eef8107c333324ea8ad37e62
tatsya-arita <k.t19860909@gmail.com>
2025-03-27 21:15:30 +0900

    Inputフォームの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import { InputForm } from './componets/Inputform'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+
+
+  const optionVal = ['優先度','高','中','低'];
+
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='inputFormContainer'>
+      <InputForm  optionVal={ optionVal }/>
+    </div>
+
+      <div className='addCreateTodoContainer'>
+        <p>○○をする</p>
+        <button>編集</button>
+        <button>完了</button>
+    </div>
+
+      <div className='finishedTodoContainer'>
+          <p>○○をした</p>
+          <button>戻す</button>
+          <button>削除</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+
+
     </>
   )
 }
